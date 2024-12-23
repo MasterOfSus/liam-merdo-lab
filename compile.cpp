51a60f81195bfbfdf35513e41ae0a5021a5cef17
MasterOfSus <cinnamontophat@gmail.com>
2024-12-23 16:51:01 +0100

    compile fix

--- compile.cpp
+++ compile.cpp
@@ -1,20 +1,18 @@
-#include <algorithm>
 void compile(const char option) {
-	gROOT->LoadMacro("particle/particleType.cpp+");
-	gROOT->LoadMacro("particle/resonanceType.cpp+");
-	gROOT->LoadMacro("particle/particle.cpp+");
-	if (option == 's') gROOT->LoadMacro("simulate.cpp+");
-	else if (option == 't') gROOT->LoadMacro("test.cpp+");
+
+	if (option == 's') {
+		gROOT->LoadMacro("particle/particleType.cpp+");
+		gROOT->LoadMacro("particle/resonanceType.cpp+");
+		gROOT->LoadMacro("particle/particle.cpp+");
+		gROOT->LoadMacro("simulate.cpp+");
+	}
+	else if (option == 't') {
+		gROOT->LoadMacro("particle/particleType.cpp+");
+		gROOT->LoadMacro("particle/resonanceType.cpp+");
+		gROOT->LoadMacro("particle/particle.cpp+");
+		gROOT->LoadMacro("test.cpp+");
+	}
 	else if (option == 'a') gROOT->LoadMacro("analyze.cpp+");
 	else std::cout << "Invalid option.";
 }
 
-void recompile(const char& option) {
-	gROOT->LoadMacro("particle/particleType.cpp++");
-	gROOT->LoadMacro("particle/resonanceType.cpp++");
-	gROOT->LoadMacro("particle/particle.cpp++");
-	if (option == 's') gROOT->LoadMacro("simulate.cpp++");
-	else if (option == 't') gROOT->LoadMacro("test.cpp++");
-	else if (option == 'a') gROOT->LoadMacro("analyze.cpp++");
-	else std::cout << "Invalid option.";
-}
